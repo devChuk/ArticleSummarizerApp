48fed221489cb37b4affac6f8dc0bccb58f2eff2
root <brian_chuk@yahoo.com>
2016-03-27 05:37:28 -0400

    SQL converted! :sleeping: Now setting up article view...

--- SourceCode/Finch/app/src/main/java/articlesummarizer/finch/finch/ArticleListSQLHelper.java
+++ SourceCode/Finch/app/src/main/java/articlesummarizer/finch/finch/ArticleListSQLHelper.java
@@ -9,7 +9,12 @@ public class ArticleListSQLHelper extends SQLiteOpenHelper{
 
     public static final String DB_NAME = "articlesummarizer.finch.finch";
     public static final String TABLE_NAME = "ARTICLE_LIST";
-    public static final String COL1_TASK = "article";
+
+    public static final String COL1_TASK = "title";
+    public static final String COL2_TASK = "url";
+    public static final String COL3_TASK = "author";
+    public static final String COL4_TASK = "publishdate";
+    public static final String COL5_TASK = "summary";
     public static final String _ID = BaseColumns._ID;
 
     public ArticleListSQLHelper(Context context) {
@@ -19,8 +24,14 @@ public class ArticleListSQLHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase sqlDB) {
-        String createTodoListTable = "CREATE TABLE " + TABLE_NAME + " ( _id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                COL1_TASK + " TEXT)";
+        String createTodoListTable = "CREATE TABLE " + TABLE_NAME +
+                " ( _id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL1_TASK + " TEXT," +
+                COL2_TASK + " TEXT," +
+                COL3_TASK + " TEXT," +
+                COL4_TASK + " TEXT," +
+                COL5_TASK + " TEXT)";
+//                COL1_TASK + " TEXT)";
         sqlDB.execSQL(createTodoListTable);
     }
 
--- SourceCode/Finch/app/src/main/java/articlesummarizer/finch/finch/MainActivity.java
+++ SourceCode/Finch/app/src/main/java/articlesummarizer/finch/finch/MainActivity.java
@@ -5,16 +5,34 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends ListActivity {
 
     private ListAdapter articleListAdapter;
     private ArticleListSQLHelper articleListSQLHelper;
+
+    RequestQueue queue;
+    // Tag used to log messages
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     EditText urlInput;
 
@@ -24,6 +42,9 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         updateTodoList();
+
+        // Setup Volley networking request
+        queue = Volley.newRequestQueue(this); // Need to set up a queue that holds all Volley requests
 
         urlInput = (EditText) findViewById(R.id.editText);
     }
@@ -35,33 +56,18 @@ public class MainActivity extends ListActivity {
 
         //cursor to read article list from database
         Cursor cursor = sqLiteDatabase.query(ArticleListSQLHelper.TABLE_NAME,
-                new String[]{ArticleListSQLHelper._ID, ArticleListSQLHelper.COL1_TASK},
+                new String[]{ArticleListSQLHelper._ID, ArticleListSQLHelper.COL1_TASK, ArticleListSQLHelper.COL2_TASK},
                 null, null, null, null, null);
 
         articleListAdapter = new SimpleCursorAdapter(
                 this, R.layout.list_item,
                 cursor,
-                new String[]{ArticleListSQLHelper.COL1_TASK},
-                new int[]{R.id.title},
+                new String[]{ArticleListSQLHelper.COL1_TASK, ArticleListSQLHelper.COL2_TASK},
+                new int[]{R.id.title, R.id.subtitle},
                 0
         );
 
         this.setListAdapter(articleListAdapter);
-    }
-
-    public void onSummarize(View view) {
-        String urlStringInput = urlInput.getText().toString();
-        articleListSQLHelper = new ArticleListSQLHelper(MainActivity.this);
-        SQLiteDatabase sqLiteDatabase = articleListSQLHelper.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.clear();
-
-        //write the article input into database table
-        values.put(ArticleListSQLHelper.COL1_TASK, urlStringInput);
-        sqLiteDatabase.insertWithOnConflict(ArticleListSQLHelper.TABLE_NAME, null, values, SQLiteDatabase.CONFLICT_IGNORE);
-
-        //update the article list UI
-        updateTodoList();
     }
 
     public void onDelete(View view) {
@@ -77,4 +83,111 @@ public class MainActivity extends ListActivity {
         sqlDB.execSQL(deleteArticleItemSql);
         updateTodoList();
     }
+
+    public void onSummarize(View view) {
+        final String urlStringInput = urlInput.getText().toString();
+
+        StringRequest extractReq = new StringRequest(Request.Method.POST,"https://api.aylien.com/api/v1/extract",
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject obj = new JSONObject(response);
+                        Log.d("extractReq", obj.toString());
+
+
+                        articleListSQLHelper = new ArticleListSQLHelper(MainActivity.this);
+                        SQLiteDatabase sqLiteDatabase = articleListSQLHelper.getWritableDatabase();
+                        ContentValues values = new ContentValues();
+                        values.clear();
+
+                        //write the article input into database table
+                        values.put(ArticleListSQLHelper.COL1_TASK, obj.get("title").toString());
+                        values.put(ArticleListSQLHelper.COL2_TASK, urlStringInput);
+                        values.put(ArticleListSQLHelper.COL3_TASK, obj.get("author").toString());
+                        values.put(ArticleListSQLHelper.COL4_TASK, obj.get("publishDate").toString());
+                        values.put(ArticleListSQLHelper.COL5_TASK, "ayy lmao");
+
+                        sqLiteDatabase.insertWithOnConflict(ArticleListSQLHelper.TABLE_NAME, null, values, SQLiteDatabase.CONFLICT_IGNORE);
+
+                        //update the article list UI
+                        updateTodoList();
+
+                    } catch (Throwable t) {
+                        Log.e("My App", "Could not parse malformed JSON: \"" + response + "\"");
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d(TAG, error.toString(), error);
+                }
+            }
+        ){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<>();
+                params.put("url", urlStringInput); //"https://medium.com/@zan2434/how-i-learned-to-code-d93260baf219#.6w4wtfch7"
+                return params;
+            }
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("X-AYLIEN-TextAPI-Application-Key","ac6f5d725a9c90e8373c77c233f2273e");
+                params.put("X-AYLIEN-TextAPI-Application-ID","6432fd32");
+                return params;
+            }
+        };
+        queue.add(extractReq);
+
+
+
+        /////////////////////////////////////////////////////////////////////
+
+        ////////////////////////////////////////////////////////////////////
+    }
 }
+/*
+        StringRequest sr = new StringRequest(Request.Method.POST,"https://api.aylien.com/api/v1/summarize",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject obj = new JSONObject(response);
+                            Log.d("My App", obj.toString());
+                            textView.setText(obj.get("sentences").toString());
+                        } catch (Throwable t) {
+                            Log.e("My App", "Could not parse malformed JSON: \"" + response + "\"");
+                        }
+
+//                        Toast.makeText(
+//                                getApplicationContext(),
+//                                response,
+//                                Toast.LENGTH_SHORT)
+//                                .show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, error.toString(), error);
+            }
+        }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<>();
+                params.put("sentences_number","20");
+                params.put("url",editText.getText().toString()); //"https://medium.com/@zan2434/how-i-learned-to-code-d93260baf219#.6w4wtfch7"
+
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("X-AYLIEN-TextAPI-Application-Key","ac6f5d725a9c90e8373c77c233f2273e");
+                params.put("X-AYLIEN-TextAPI-Application-ID","6432fd32");
+                return params;
+            }
+        };
+        queue.add(sr);
+ */
